5de32fc13914b83d94675b0d0fd3b8ac28ab1872
Tianjian Wang <wangtjwork@gmail.com>
2018-04-21 22:33:29 -0500

    feat: Render tweetId in Dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,7 +6,16 @@ class Dashboard extends Component {
     console.log(this.props);
     return (
       <div>
-        Dashboard
+        <h3 className="center">
+          Your Timeline
+        </h3>
+        <ul className="dashboard-list">
+          {this.props.tweetIds.map(id => (
+            <li key={id}>
+              <div>TWEET ID: {id}</div>
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }
