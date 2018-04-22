a28d53220099194a68b71ec240a550f66e739f07
Tianjian Wang <wangtjwork@gmail.com>
2018-04-21 23:35:02 -0500

    feat: Complete add data to Tweet component

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,7 +4,12 @@ import { formatTweet } from '../utils/helpers'
 
 class Tweet extends Component {
   render() {
-    console.log(this.props);
+    const { tweet } = this.props;
+
+    if (tweet === null) {
+      return <p>This tweet doesn't exist.</p>
+    }
+
     return (
       <div className="tweet"></div>
     )
@@ -13,10 +18,11 @@ class Tweet extends Component {
 
 const mapStateToProps = ({ users, tweets, authedUser }, {id}) => {
   const tweet = tweets[id];
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
 
   return {
     authedUser,
-    tweet: formatTweet(tweet, users[tweet.author], authedUser)
+    tweet: tweet ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet) : null
   }
 }
 
