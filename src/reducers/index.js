1f5a97aca5170b7eebb522af3e6427957b360bcf
Tianjian Wang <wangtjwork@gmail.com>
2018-04-22 18:07:20 -0500

    feat: Add like action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import users from './users'
 import tweets from './tweets'
 import authedUser from './authedUser'
+import { loadingBarReducer } from 'react-redux-loading'
 
 export default combineReducers({
   users,
   tweets,
-  authedUser
+  authedUser,
+  loadingBar: loadingBarReducer
 })
