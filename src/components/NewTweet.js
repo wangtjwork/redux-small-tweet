09e7a5cc43c2095fb5b009b0ca1eb90ddedc49bf
Tianjian Wang <wangtjwork@gmail.com>
2018-04-22 20:37:15 -0500

    feat: Add methods to NewTweet component

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 
 class NewTweet extends Component {
+  state = {
+    text: ''
+  }
+
+  handleChange = (e) => {
+    const text =  e.target.value;
+
+    this.setState(() => ({
+      text
+    }));
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { text } = this.state;
+
+    // todo: Add state to store
+    console.log('New tweet is: ', text);
+  }
+
   render() {
     return (
       <div>New Tweet</div>
