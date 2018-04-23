b08ce2a928d390dcd619318857b3bbd4cd223948
Tianjian Wang <wangtjwork@gmail.com>
2018-04-22 21:00:34 -0500

    feat: Add UI to NewTweet component

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -23,8 +23,38 @@ class NewTweet extends Component {
   }
 
   render() {
+    const { text } = this.state;
+
+    const tweetLeft = 280 - text.length;
+
     return (
-      <div>New Tweet</div>
+      <div>
+        <h3 className="center">
+          Compose New Tweet
+        </h3>
+
+        <form className="new-tweet" onSubmit={this.handleSubmit}>
+          <textarea
+            placeholder="What's happening?"
+            value={text}
+            onChange={this.handleChange}
+            className="textarea"
+            maxLength={280}
+          />
+          {tweetLeft <= 100 && (
+            <div className="tweet-length">
+              {tweetLeft}
+            </div>
+          )}
+          <button
+            className="btn"
+            type="submit"
+            disabled={text === ''}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     )
   }
 }
