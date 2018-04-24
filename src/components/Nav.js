b25d20bdef0144c452c09960f87fb1a3b7f60aa4
Tianjian Wang <wangtjwork@gmail.com>
2018-04-24 11:44:02 -0500

    feat: Add Routes and Nav to App

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { NavLink } from 'react-router';
+import { NavLink } from 'react-router-dom';
 
-export default function NavLink() {
+export default function Nav() {
   return (
     <nav className="nav">
       <ul>
