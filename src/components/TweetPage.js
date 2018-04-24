12ab9799a15062b8995b3b7994da4692345d004f
Tianjian Wang <wangtjwork@gmail.com>
2018-04-23 21:04:42 -0500

    feat: Add UI to TweetPage

--- src/components/TweetPage.js
+++ src/components/TweetPage.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Tweet from './Tweet';
+import NewTweet from './NewTweet'
 
 class TweetPage extends Component {
   render() {
-    console.log(this.props);
+    const { id, replies } = this.props;
+
 
     return (
       <div>
-        TweetPage
+        <Tweet id={id} />
+        <NewTweet id={id} />
+        {replies.length !== 0 && <h3 className="center">Replies</h3>}
+        <ul>
+          {replies.map((replyId) => (
+            <li key={replyId}>
+              <Tweet id={replyId}/>
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }
