becafa13ff2dc2431f4cfe0e19f2daa917bb48ff
Tianjian Wang <wangtjwork@gmail.com>
2018-04-23 20:58:17 -0500

    feat: Add connect to TweetPage

--- src/components/TweetPage.js
+++ src/components/TweetPage.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class TweetPage extends Component {
   render() {
+    console.log(this.props);
+
     return (
       <div>
         TweetPage
@@ -10,4 +13,15 @@ class TweetPage extends Component {
   }
 }
 
-export default TweetPage;
+const mapStateToProps = ({ authedUser, tweets, users }, ownProps) => {
+  const { id } = ownProps.match.params;
+
+  return {
+    id,
+    replies: !tweets[id]
+      ? []
+      : tweets[id].replies.sort((a, b) => tweets[b].timestamp - tweets[a].timestamp),
+  }
+}
+
+export default connect(mapStateToProps)(TweetPage);
