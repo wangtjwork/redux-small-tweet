013b4645e98d13480c60dd853f451b606ee59726
Tianjian Wang <wangtjwork@gmail.com>
2018-04-23 20:33:50 -0500

    feat: Add TweetPage component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
 import LoadingBar from 'react-redux-loading'
 import NewTweet from './NewTweet'
+import TweetPage from './TweetPage'
 
 class App extends Component {
   componentDidMount() {
@@ -17,7 +18,7 @@ class App extends Component {
         {
           this.props.loading === true
           ? null
-          : <NewTweet />
+          : <TweetPage />
         }
       </div>
     )
---
+++ src/components/TweetPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TweetPage extends Component {
+  render() {
+    return (
+      <div>
+        TweetPage
+      </div>
+    )
+  }
+}
+
+export default TweetPage;
